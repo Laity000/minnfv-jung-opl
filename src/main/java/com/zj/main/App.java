3c76b09030a30c85464f1ecad221fa57e0d76de7
ZhangJing <625740704@qq.com>
2018-03-05 16:50:02 +0800

    beta V0.4 add data file

--- src/main/java/com/zj/main/App.java
+++ src/main/java/com/zj/main/App.java
@@ -13,10 +13,10 @@ import com.zj.test.Test1;
 public class App
 {
 	//static String path = "C:\\Users\\PC\\Desktop\\result_minnfv.json";
-	//static String path = "C:\\Users\\PC\\Desktop\\data\\case\\case2-6-5-4.json";
-	//static String path = "C:\\Users\\PC\\opl\\minlink\\result_minlink.json";
+	static String path = "C:\\Users\\PC\\Desktop\\data\\case\\case-ram.json";
+	//static String path = "C:\\Users\\PC\\opl\\minnfv\\result_minnfv.json";
 	//static String path = "C:\\Users\\PC\\Desktop\\data\\uncut\\ram93-d-2-70.json";
-	static String path = "C:\\Users\\PC\\Desktop\\data\\newyork\\new56-1-t-1000-6-4-3.json";
+	//static String path = "C:\\Users\\PC\\Desktop\\data\\newyork\\new56-1-t-1000-6-4-3.json";
 	//static String path = "C:\\Users\\PC\\Desktop\\data\\pdh\\new4\\pdh18-int-t-30-6-4-3.json";
 	//static String path = "C:\\Users\\PC\\Desktop\\data\\pdh\\new4\\h-pdh18-int.json";
 	//static String path = "C:\\Users\\PC\\Desktop\\data\\newyork\\h-new-56-1.json";
